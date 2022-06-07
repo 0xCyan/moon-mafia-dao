1d7b6a07e9131ebb7481db65f508d6c68e22918f
Drew Sadik <0xCyan@protonmail.com>
2022-06-07 11:07:52 -0400

    Allow user to connect wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 const App = () => {
+
+  // Third web hooks!
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to Moon Mafia DAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect Wallet or Die Trying
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet Connected, You Live to See Another Day!</h1>
     </div>
   );
-};
+}
 
 export default App;
